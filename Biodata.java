23e096f5898a0c1e53b3dea8feb38a8391e26c65
bily81 <72961580+bily81@users.noreply.github.com>
2020-12-27 22:18:22 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AlurJavaDasar;
+
+/**
+ *
+ * @author Hp Pavillion 14
+ */
+public class Biodata {
+    public static void main(String [] Args){
+    
+    String Nama="MUHAMMAD SABILILAH SYAHBANA";
+    String JK="LAKI-LAKI";
+    String Alamat="DUSUN KUCUR DESA SUMBEREJO PURWOSARI";
+    String Jurusan="REKAYASA PERANGKAT LUNAK";
+        
+    System.out.println("BIODATA DIRI");
+    System.out.println("------------");
+    System.out.println("Nama \t\t\t\t:"+Nama);
+    System.out.println("Jenis Kelamin \t\t\t:"+JK);
+    System.out.println("Almat \t\t\t\t:"+Alamat);
+    System.out.println("Jurusan \t\t\t:"+Jurusan);
+    
+    }
+    
+}
